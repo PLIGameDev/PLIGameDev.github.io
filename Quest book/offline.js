1246c27c8fb97838b020afaace0c4f37bbd943a2
JonaszO <delgado88a@o2.pl>
2017-08-16 22:44:35 +0200

    m

---
+++ Quest book/offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1502916208,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/woodtable-sheet0.png",
+		"images/scrollablefieldoftext-sheet0.png",
+		"images/mask-sheet0.png",
+		"images/frame.png",
+		"images/scroll_position-sheet0.png",
+		"images/diarytext.png",
+		"images/tiledbackground.png"
+	]
+}
