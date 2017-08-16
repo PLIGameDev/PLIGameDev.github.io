4867e9a0885a044309668028cd67ae25b776ce20
JonaszO <delgado88a@o2.pl>
2017-08-16 23:45:50 +0200

    d

---
+++ lemmings/offline.js
@@ -0,0 +1,16 @@
+﻿{
+	"version": 1502919914,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/canvas.png",
+		"images/lemming-sheet0.png",
+		"images/lemming-sheet1.png",
+		"images/holemaker-sheet0.png",
+		"images/holemaker-sheet1.png",
+		"images/level-sheet0.png",
+		"images/spawner-sheet0.png"
+	]
+}
