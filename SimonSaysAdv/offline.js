9446d99e3c81c89301147a73f8c08b0e702e653b
JonaszO <delgado88a@o2.pl>
2017-08-17 14:03:51 +0200

    e

---
+++ SimonSaysAdv/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1502971322,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/cpad-sheet0.png",
+		"images/cpad-sheet1.png",
+		"media/score.m4a",
+		"media/score.ogg",
+		"media/failure.m4a",
+		"media/failure.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
