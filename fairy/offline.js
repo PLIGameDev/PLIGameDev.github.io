b985485d291424cc672c0513eaa7a0f7062f9b93
JonaszO <delgado88a@o2.pl>
2017-08-23 22:39:57 +0200

    f

---
+++ fairy/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1503520777,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/fairy-sheet0.png",
+		"images/bg.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
