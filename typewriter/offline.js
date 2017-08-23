38113452d2760cd076ae498480916abd9b97e34c
JonaszO <delgado88a@o2.pl>
2017-08-23 20:08:13 +0200

    d

---
+++ typewriter/offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1503502578,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"media/160678__bmaczero__typewriter.m4a",
+		"media/160678__bmaczero__typewriter.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
