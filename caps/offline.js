271d64837353c1c78d0a03de75b9fa927ab4976e
JonaszO <delgado88a@o2.pl>
2017-08-23 20:08:25 +0200

    d

---
+++ caps/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1503511659,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/yellow-sheet0.png",
+		"images/spwall-sheet0.png",
+		"images/spball-sheet0.png",
+		"images/sparrow-sheet0.png",
+		"images/red-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
