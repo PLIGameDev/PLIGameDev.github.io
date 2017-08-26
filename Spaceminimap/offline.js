2bab8fb3a2550b10938361d14b2a70992a4aed62
JonaszO <delgado88a@o2.pl>
2017-08-26 14:17:34 +0200

    l

---
+++ Spaceminimap/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1503749802,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/map-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/enemy_icon-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player_icon-sheet0.png",
+		"images/stationicon-sheet0.png",
+		"images/food-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
