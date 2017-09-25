02ec7c0069af6881df626bd1c5d620fc315dd627
JonaszO <delgado88a@o2.pl>
2017-09-25 12:34:07 +0200

    l

---
+++ limuzya/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1506335639,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/carblack-sheet0.png",
+		"images/33864_deformable_fiat_seicento-sheet0.png",
+		"images/particles.png",
+		"images/ccpolognee-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground.png",
+		"images/particles2.png",
+		"media/samochod rusza.m4a",
+		"media/samochod rusza.ogg",
+		"media/samochod rusza2.m4a",
+		"media/samochod rusza2.ogg",
+		"media/motor.m4a",
+		"media/motor.ogg",
+		"media/motor2.m4a",
+		"media/motor2.ogg",
+		"media/carhit.m4a",
+		"media/carhit.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
